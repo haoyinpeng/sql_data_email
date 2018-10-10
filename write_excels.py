ce3190e6ff12be5bc54bea7ce7f7363889f0b1af
haoyinpeng <haopeng@pro-os.local>
2018-10-10 18:20:16 +0800

    将WriteExcel改写成类 名称 title都为实例属性

--- write_excels.py
+++ write_excels.py
@@ -1,13 +1,13 @@
 
 
-import openpyxl
+from openpyxl import Workbook
 import process_data
 import datetime
 import sqls
 
 def write_excel(user,passwd):
 	#create a workbook
-	wb = openpyxl.Workbook()
+	wb = Workbook()
 	#create a sheet
 	sheet1 = wb.create_sheet(index=0,title='outresource')
 	results_all = process_data.get_test_datas(user,passwd)
@@ -23,4 +23,16 @@ def write_excel(user,passwd):
 	name_str = 'w_resouece_'+str(datetime.datetime.now().strftime('%Y%m%d%H%I%S'))+'.xlsx'
 	wb.save(name_str)
 	return name_str
+
+class WriteExcel(Workbook):
+	def __init__(self,user,passwd,data_kind,data_name,data_title):
+		print('__init__')
+		self.user = user
+		self.passwd = passwd
+		self.type = data_kind
+		self.data_name = data_name
+		self.data_title = data_title
+
+	def write_excel(self):
+		pass
 
