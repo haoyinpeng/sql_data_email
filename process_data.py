2bdfca881957221a2d0b31413175a2631c821fda
haoyinpeng <haopeng@pro-os.local>
2018-10-15 17:37:29 +0800

    delete others function

--- process_data.py
+++ process_data.py
@@ -12,15 +12,6 @@ import datetime
 SQL_STR = sqls.W_RESOURCE
 SQL_PARAMS = sqls.W_PARAMS
 
-
-if sys.argv[2] is not None and sys.argv[2]=='W':
-	SQL_STR = sqls.W_RESOURCE
-	SQL_PARAMS = sqls.W_PARAMS
-elif sys.argv[2] is not None and sys.argv[2]=='..':
-	pass
-
-
-
 def get_pro_datas(user,passwd):
 	#tns
 	tns = oracle.makedsn('172.18.2.52',1521,'perp1')
@@ -29,12 +20,6 @@ def get_pro_datas(user,passwd):
 	#cursor
 	cursor = db.cursor()
 	print('cursor...')
-
-	if sys.argv[0] == 'outresource':
-		#params = {'resource_code':str(sys.argv[1])}
-		print('sys.argv[0]' + sys.argv[0])
-		print('sys.argv[1]' + sys.argv[1])
-
 	#execute sql
 	print('cursor execute...')
 	cursor.execute(SQL_STR,SQL_PARAMS)
@@ -73,25 +58,7 @@ def get_test_datas(user,passwd):
 	print('db.close()...')
 	return result_all
 
-def write_excel():
-	#create a workbook
-	wb = openpyxl.Workbook()
-	#create a sheet
-	sheet1 = wb.create_sheet(index=0,title='outresource')
-	results_all = get_test_datas('','')
-	#results_all = get_pro_datas()
-	print('add title...')
-	sheet1.append(sqls.W_TITLE)
-	print('add lines...')
-	for i in results_all:
-		sheet1.append(i)
-
-    #sheet1.add_table()
-	print('save excel...')
-	name_str = 'w_resouece_'+str(datetime.datetime.now().strftime('%Y%m%d%H%I%S'))+'.xlsx'
-	wb.save(name_str)
-	return name_str
-
 
 if __name__ == '__main__':
-	write_excel()
+	get_test_datas('','')
+
